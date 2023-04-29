30ca844c5b4c24b4211e8924d9ed646056d7825e
Shu Wang <shugewang@hotmail.co.uk>
2023-04-29 19:53:00 +0100

    Metadata and styling

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I am Magneto, the Master of Magnetism and a powerful mutant with the ability to manipulate magnetic fields. I have dedicated my life to fighting for mutant rights, and I will stop at nothing to protect and empower my fellow mutant.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
